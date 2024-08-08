7a5a25642a5efdf35c683b76df8e31d83381fd24
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-08-07 21:53:16 -0700

    remove dark mode features

--- my-portfolio/components/RetroGrid.tsx
+++ my-portfolio/components/RetroGrid.tsx
@@ -19,14 +19,12 @@ export default function RetroGrid({ className }: { className?: string }) {
             // Light Styles
             "[background-image:linear-gradient(to_right,rgba(0,0,0,0.3)_1px,transparent_0),linear-gradient(to_bottom,rgba(0,0,0,0.3)_1px,transparent_0)]",
 
-            // Dark styles
-            "dark:[background-image:linear-gradient(to_right,rgba(255,255,255,0.2)_1px,transparent_0),linear-gradient(to_bottom,rgba(255,255,255,0.2)_1px,transparent_0)]",
           )}
         />
       </div>
 
       {/* Background Gradient */}
-      <div className="absolute inset-0 bg-gradient-to-t from-white to-transparent to-90% dark:from-black" />
+      <div className="absolute inset-0 bg-gradient-to-t from-white to-transparent to-90%" />
     </div>
   );
 }
