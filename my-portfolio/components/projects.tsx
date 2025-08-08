0bd2589a32195be76ec1a84081a34c21683d91bf
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2025-08-07 22:53:36 -0700

    update projects, added co-draw and harbor

--- my-portfolio/components/projects.tsx
+++ my-portfolio/components/projects.tsx
@@ -9,10 +9,17 @@ import SQLite_Logo from "@/assets/SQLiteLogo.png"
 import PostgreSQL_Logo from "@/assets/PostgreSQL.png"
 import FastAPI_Logo from "@/assets/FastAPI.png"
 import React_Logo from "@/assets/ReactLogo.png"
+import ReactNative from "@/assets/ReactNative.svg"
+import Expo from "@/assets/Expo.png"
+import Svelte from "@/assets/svelte.png"
+import SpringBoot from "@/assets/springboot.png"
 import Jupyter_Logo from "@/assets/JupyterLogo.png"
 import FirebaseLogo from "@/assets/FirebaseLogo.png"
+import Supabase from "@/assets/Supabase.jpg"
 import Occurrence_App from '@/assets/Occurrence_App.png'
 import ScavangerHunt from '@/assets/ScavangerHunt.png'
+import Harbor from '@/assets/Harbor_App.png'
+import Co_Draw from '@/assets/Co-Draw_App.png'
 import Link from "next/link";
 
 export function BentoGridProjects() {
@@ -35,6 +42,43 @@ const Skeleton = () => (
   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
 );
 const items = [
+
+  {
+    title: "Harbor",
+    description: "A full stack web app created during HackDavis 2025 using React Native, Expo, and Supabase.",
+    header: <Link href={"https://devpost.com/software/harbor-9kq0e6"} target="_blank" className="flex flex-1 w-full h-full min-h-[6rem]" >
+                <Image
+                    src={Harbor}
+                    alt="Image of the harbor app submission"
+                    className="flex flex-1 w-full h-full min-h-[6rem]"
+                    />
+            </Link>,
+    className: "md:col-span-1 p-2 m-3 sm:m-0",
+    icon: <div className="flex flex-wrap gap-2 items-center justify-center">
+        <Image src={ReactNative} alt="React Native logo" className="w-auto h-8" />
+        <Image src={Expo} alt="Figma logo" className="w-auto h-8" />
+        <Image src={Supabase} alt="Supabase logo" className="w-auto h-8" />
+    </div>
+  },
+
+  {
+    title: "Co-Draw",
+    description: "A real-time collaborative drawing app developed independently using Svelte, and Spring Boot.",
+    header: <Link href={"https://github.com/Rani-Codes/Co-Draw"} target="_blank" className="flex flex-1 w-full h-full min-h-[6rem]" >
+                <Image
+                    src={Co_Draw}
+                    alt="Image of the co-draw app"
+                    className="flex flex-1 w-full h-full min-h-[6rem]"
+                    />
+            </Link>,
+    className: "md:col-span-2 p-2 m-3 sm:m-0",
+    icon: <div className="flex flex-wrap gap-2 items-center justify-center">
+        <Image src={Svelte} alt="Svelte logo" className="w-auto h-8" />
+        <Image src={Tailwind_Logo} alt="Tailwind logo" className="w-auto h-4" />
+        <Image src={SpringBoot} alt="Spring Boot logo" className="w-auto h-8" />
+    </div>
+  },
+
   {
     title: "Todo List",
     description: "A full stack web app created independently to learn FastAPI and implement user authentication.",
